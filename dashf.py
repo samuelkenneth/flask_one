bd51d42cf313862350cacf96a285cf746d8755ca
Samuel Kenneth <samuelkennethc137@gmail.com>
2021-10-01 11:13:07 +0530

    made changes in dashf.py

--- dashf.py
+++ dashf.py
@@ -5,9 +5,11 @@ app = Flask(__name__) #creating the Flask class object
  
 @app.route('/') #decorator drfines the   
 def home():  
-    return "hello, this is our first flask website";  
+    return "hello, this is our first flask website 2";  
 
 
 
 if __name__ =='__main__':  
     app.run(debug = True)  
+
+# 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
