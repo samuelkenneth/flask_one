66d72ae9057ad44151e169a31196f10a7505942e
Samuel Kenneth <samuelkennethc137@gmail.com>
2021-10-01 10:28:24 +0530

    first commit

---
+++ dashf.py
@@ -0,0 +1,13 @@
+from flask import Flask  
+import re
+  
+app = Flask(__name__) #creating the Flask class object   
+ 
+@app.route('/') #decorator drfines the   
+def home():  
+    return "hello, this is our first flask website";  
+
+
+
+if __name__ =='__main__':  
+    app.run(debug = True)  
---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+from dashf import app
+class BasicTestCase(unittest.TestCase):
+    def test_home(self):
+            tester = app.test_client(self)
+            response = tester.get('/', content_type='html/text')
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.data, b'hello, this is our first flask website')
+if __name__ == '__main__':
+    unittest.main()
